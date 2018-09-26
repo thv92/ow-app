86109fa90ec162166f6400569785752a13e211a9
Thien Vo <vohuythien@utexas.edu>
2018-09-26 18:44:11 -0500

    Skeleton push

---
+++ webpack.dev.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: ['babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+
+
+    }
+};
